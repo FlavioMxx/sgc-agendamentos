8e27ac35a9217fe586a51039911394438d00435f
Flavio Milani <frmilanij@gmail.com>
2025-01-25 20:21:02 -0300

    feat: Corrige imports para novos dtos, adiciona mensagem de erro caso buscarEstabelecimentos seja vazio e cria metodo de atualizar estabelecimento

--- src/main/java/br/com/fmxx/sgc_agendamento/service/EstabelecimentoService.java
+++ src/main/java/br/com/fmxx/sgc_agendamento/service/EstabelecimentoService.java
@@ -1,6 +1,7 @@
 package br.com.fmxx.sgc_agendamento.service;
 
-import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoDTO;
+import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoRequestDTO;
+import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoResponseDTO;
 import br.com.fmxx.sgc_agendamento.entity.Estabelecimento;
 import br.com.fmxx.sgc_agendamento.exceptions.EntidadeInexistenteException;
 import br.com.fmxx.sgc_agendamento.mapper.EstabelecimentoMapper;
@@ -18,21 +19,38 @@ public class EstabelecimentoService {
     private final EstabelecimentoRepository repository;
     private final EstabelecimentoMapper mapper;
 
-    public EstabelecimentoDTO criarEstabelecimento(EstabelecimentoDTO estabelecimentoDTO) throws IllegalAccessException {
+    public EstabelecimentoResponseDTO criarEstabelecimento(EstabelecimentoRequestDTO estabelecimentoRequestDTO) throws IllegalAccessException {
 
-        DadosInvalidosValidator.validar(estabelecimentoDTO);
+        DadosInvalidosValidator.validar(estabelecimentoRequestDTO);
 
-        Estabelecimento novoEstabelecimento = repository.save(mapper.dtoToEntity(estabelecimentoDTO));
+        Estabelecimento novoEstabelecimento = repository.save(mapper.dtoToEntity(estabelecimentoRequestDTO));
 
-        return mapper.entityToDto(novoEstabelecimento);
+        return mapper.entityToResponseDTO(novoEstabelecimento);
     }
 
-    public List<EstabelecimentoDTO> buscarEstabelecimentos() {
+    public EstabelecimentoResponseDTO atualizarEstabelecimento(Long id, EstabelecimentoRequestDTO estabelecimentoRequestDTO) throws IllegalAccessException {
+        Estabelecimento estabelecimento = repository.findById(id)
+                .orElseThrow(() -> new EntidadeInexistenteException("Estabelecimento nao existe: id" + id));
+
+        DadosInvalidosValidator.validar(estabelecimentoRequestDTO);
+
+        mapper.updateEstabelecimentoFromDTO(estabelecimentoRequestDTO, estabelecimento);
+
+        repository.save(estabelecimento);
+
+        return mapper
+                .entityToResponseDTO(estabelecimento);
+    }
+
+    public List<EstabelecimentoResponseDTO> buscarEstabelecimentos() {
         List<Estabelecimento> estabelecimentoList = repository.findAll();
+
+        if(estabelecimentoList.isEmpty())
+            throw new EntidadeInexistenteException("Nao existem estabelecimentos cadastrados");
 
         return estabelecimentoList
                 .stream()
-                .map(mapper::entityToDto)
+                .map(mapper::entityToResponseDTO)
                 .toList();
     }
 
