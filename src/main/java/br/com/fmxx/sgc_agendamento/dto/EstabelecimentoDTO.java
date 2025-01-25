3aab8071a534f87ef68e5e51b1e6265dad88a817
Flavio Milani <frmilanij@gmail.com>
2025-01-25 19:19:44 -0300

    feat: Adicionado anotacao para ignorar validacao de dados do campo

--- src/main/java/br/com/fmxx/sgc_agendamento/dto/EstabelecimentoDTO.java
+++ src/main/java/br/com/fmxx/sgc_agendamento/dto/EstabelecimentoDTO.java
@@ -1,9 +1,13 @@
 package br.com.fmxx.sgc_agendamento.dto;
 
+import br.com.fmxx.sgc_agendamento.validator.IgnorarDadosValidator;
+
 public record EstabelecimentoDTO(
+        @IgnorarDadosValidator
         Long id,
         String nomeEstabelecimento,
         String enderecoEstabelecimento,
         String contatoEstabelecimento,
-        Boolean ativo) {
-}
+        Boolean ativo
+)
+{ }
