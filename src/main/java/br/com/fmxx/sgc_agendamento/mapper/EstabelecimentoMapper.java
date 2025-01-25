639f253678b0b5111dc41af3f7c8bc57fc6111bd
Flavio Milani <frmilanij@gmail.com>
2025-01-25 20:17:52 -0300

    feat: Melhora mapper para diferentes dtos e adiciona mapping de update

--- src/main/java/br/com/fmxx/sgc_agendamento/mapper/EstabelecimentoMapper.java
+++ src/main/java/br/com/fmxx/sgc_agendamento/mapper/EstabelecimentoMapper.java
@@ -1,13 +1,21 @@
 package br.com.fmxx.sgc_agendamento.mapper;
 
-import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoDTO;
+import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoRequestDTO;
+import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoResponseDTO;
 import br.com.fmxx.sgc_agendamento.entity.Estabelecimento;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface EstabelecimentoMapper {
 
-    Estabelecimento dtoToEntity(EstabelecimentoDTO dto);
+    Estabelecimento dtoToEntity(EstabelecimentoRequestDTO dto);
 
-    EstabelecimentoDTO entityToDto(Estabelecimento entity);
+    EstabelecimentoRequestDTO entityToRequestDTO(Estabelecimento entity);
+
+    EstabelecimentoResponseDTO entityToResponseDTO(Estabelecimento entity);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEstabelecimentoFromDTO(EstabelecimentoRequestDTO dto, @MappingTarget Estabelecimento estabelecimento);
 }
