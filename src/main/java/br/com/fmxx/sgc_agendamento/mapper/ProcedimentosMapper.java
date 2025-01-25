31cd7b789c3573dc875175e811783558deacf00c
Flavio Milani <frmilanij@gmail.com>
2025-01-25 19:31:32 -0300

    feat: Criado mapper para procedimentos

---
+++ src/main/java/br/com/fmxx/sgc_agendamento/mapper/ProcedimentosMapper.java
@@ -0,0 +1,21 @@
+package br.com.fmxx.sgc_agendamento.mapper;
+
+import br.com.fmxx.sgc_agendamento.dto.ProcedimentosRequestDTO;
+import br.com.fmxx.sgc_agendamento.dto.ProcedimentosResponseDTO;
+import br.com.fmxx.sgc_agendamento.entity.Procedimentos;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface ProcedimentosMapper {
+
+    Procedimentos dtoToEntity(ProcedimentosRequestDTO dto);
+
+    ProcedimentosRequestDTO entityToDto(Procedimentos entity);
+
+    ProcedimentosResponseDTO entityToResponseDTO(Procedimentos entity);
+
+    @Mapping(target = "id", ignore = true)
+    void updateProcedimentoFromDTO(ProcedimentosRequestDTO dto, @MappingTarget Procedimentos procedimento);
+}
