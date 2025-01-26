5a3bf2bbd6dd7d179dd1379cc2d5349be6dc160a
Flavio Milani <frmilanij@gmail.com>
2025-01-25 22:15:51 -0300

    feat: Adiciona logica para atribuir um estabelecimento na criacao de procedimento

--- src/main/java/br/com/fmxx/sgc_agendamento/service/ProcedimentosService.java
+++ src/main/java/br/com/fmxx/sgc_agendamento/service/ProcedimentosService.java
@@ -1,29 +1,46 @@
 package br.com.fmxx.sgc_agendamento.service;
 
+import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoResponseDTO;
 import br.com.fmxx.sgc_agendamento.dto.ProcedimentosRequestDTO;
 import br.com.fmxx.sgc_agendamento.dto.ProcedimentosResponseDTO;
+import br.com.fmxx.sgc_agendamento.entity.Estabelecimento;
 import br.com.fmxx.sgc_agendamento.entity.Procedimentos;
 import br.com.fmxx.sgc_agendamento.exceptions.EntidadeInexistenteException;
+import br.com.fmxx.sgc_agendamento.mapper.EstabelecimentoMapper;
 import br.com.fmxx.sgc_agendamento.mapper.ProcedimentosMapper;
+import br.com.fmxx.sgc_agendamento.repository.EstabelecimentoRepository;
 import br.com.fmxx.sgc_agendamento.repository.ProcedimentosRepository;
 import br.com.fmxx.sgc_agendamento.validator.DadosInvalidosValidator;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+//TODO: Converter todos os retornos para Procedimentos e fazer a conversao na CONTROLLER
 
 @Service
 @RequiredArgsConstructor
 public class ProcedimentosService {
 
     private final ProcedimentosRepository repository;
+    private final EstabelecimentoService estabelecimentoService;
+    private final EstabelecimentoMapper estabelecimentoMapper;
     private final ProcedimentosMapper mapper;
 
+    @Transactional
     public ProcedimentosResponseDTO criarProcedimentos(ProcedimentosRequestDTO procedimentosRequestDTO) throws IllegalAccessException {
 
         DadosInvalidosValidator.validar(procedimentosRequestDTO);
 
-        Procedimentos novoProcedimento = repository.save(mapper.dtoToEntity(procedimentosRequestDTO));
+        Procedimentos novoProcedimento = mapper.dtoToEntity(procedimentosRequestDTO);
+
+        EstabelecimentoResponseDTO estabelecimento = estabelecimentoService
+                .buscarEstabelecimentoPorId(procedimentosRequestDTO.idEstabelecimento());
+
+        novoProcedimento
+                .setEstabelecimento(estabelecimentoMapper.responseDTOToEntity(estabelecimento));
+
+        repository.save(novoProcedimento);
 
         return mapper
                 .entityToResponseDTO(novoProcedimento);
