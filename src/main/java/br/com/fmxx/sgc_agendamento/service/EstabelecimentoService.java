f61703034f9c44da2be6559579a1aba90b5444cc
Flavio Milani <frmilanij@gmail.com>
2025-01-25 22:15:23 -0300

    feat: Adiciona logica na criacao de estabelecimento com procedimentos

--- src/main/java/br/com/fmxx/sgc_agendamento/service/EstabelecimentoService.java
+++ src/main/java/br/com/fmxx/sgc_agendamento/service/EstabelecimentoService.java
@@ -3,6 +3,7 @@ package br.com.fmxx.sgc_agendamento.service;
 import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoRequestDTO;
 import br.com.fmxx.sgc_agendamento.dto.EstabelecimentoResponseDTO;
 import br.com.fmxx.sgc_agendamento.entity.Estabelecimento;
+import br.com.fmxx.sgc_agendamento.entity.Procedimentos;
 import br.com.fmxx.sgc_agendamento.exceptions.EntidadeInexistenteException;
 import br.com.fmxx.sgc_agendamento.mapper.EstabelecimentoMapper;
 import br.com.fmxx.sgc_agendamento.repository.EstabelecimentoRepository;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+//TODO: Converter todos os retornos para Procedimentos e fazer a conversao na CONTROLLER
+
 @Service
 @RequiredArgsConstructor
 public class EstabelecimentoService {
@@ -20,23 +23,35 @@ public class EstabelecimentoService {
     private final EstabelecimentoMapper mapper;
 
     public EstabelecimentoResponseDTO criarEstabelecimento(EstabelecimentoRequestDTO estabelecimentoRequestDTO) throws IllegalAccessException {
-
         DadosInvalidosValidator.validar(estabelecimentoRequestDTO);
 
-        Estabelecimento novoEstabelecimento = repository.save(mapper.dtoToEntity(estabelecimentoRequestDTO));
+        Estabelecimento estabelecimento = mapper.requestDTOToEntity(estabelecimentoRequestDTO);
 
-        return mapper.entityToResponseDTO(novoEstabelecimento);
+        for (Procedimentos procedimento : estabelecimento.getProcedimentos()) {
+            procedimento.setEstabelecimento(estabelecimento);
+        }
+
+        estabelecimento = repository.save(estabelecimento);
+
+        return mapper.entityToResponseDTO(estabelecimento);
     }
 
     public EstabelecimentoResponseDTO atualizarEstabelecimento(Long id, EstabelecimentoRequestDTO estabelecimentoRequestDTO) throws IllegalAccessException {
-        Estabelecimento estabelecimento = repository.findById(id)
-                .orElseThrow(() -> new EntidadeInexistenteException("Estabelecimento nao existe: id" + id));
+        Estabelecimento estabelecimento = mapper.responseDTOToEntity(this.buscarEstabelecimentoPorId(id));
 
         DadosInvalidosValidator.validar(estabelecimentoRequestDTO);
 
         mapper.updateEstabelecimentoFromDTO(estabelecimentoRequestDTO, estabelecimento);
 
         repository.save(estabelecimento);
+
+        return mapper
+                .entityToResponseDTO(estabelecimento);
+    }
+
+    public EstabelecimentoResponseDTO buscarEstabelecimentoPorId(Long id) {
+        Estabelecimento estabelecimento = repository.findById(id)
+                .orElseThrow(() -> new EntidadeInexistenteException("Estabelecimento nao existe: id" + id));
 
         return mapper
                 .entityToResponseDTO(estabelecimento);
